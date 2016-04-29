9c452967014ad557e92ab070121a9bd09a35bce1
yuhan <yud039@ucsd.edu>
2016-04-28 20:38:27 -0700

    cleaned up, working code ready to merge into master

--- app/src/main/java/com/cs110/lit/adventour/MapsActivity.java
+++ app/src/main/java/com/cs110/lit/adventour/MapsActivity.java
@@ -53,28 +53,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         /////---------- permission check -------------////
         // Register the listener with the Location Manager to receive location updates
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
 
         String locationNetworkProvider = LocationManager.NETWORK_PROVIDER;
-        String locationGPSProvider = LocationManager.GPS_PROVIDER;
-
-        //locationManager.requestLocationUpdates(locationNetworkProvider, 0, 0, (android.location.LocationListener) locationListener);
-        //locationManager.requestLocationUpdates(locationNetworkProvider, 0, 0, (android.location.LocationListener) locationListener);
-        //locationManager.requestLocationUpdates(locationGPSProvider, 0, 0, (android.location.LocationListener) locationListener);
-        // Remove the listener you previously added
-        //locationManager.removeUpdates((android.location.LocationListener) locationListener);
-
-
         Location lastKnownLocation = locationManager.getLastKnownLocation(locationNetworkProvider);
-
 
         ////----------- display the map with marker on current location -----------//
         // Add a marker in current location, and move the camera.
