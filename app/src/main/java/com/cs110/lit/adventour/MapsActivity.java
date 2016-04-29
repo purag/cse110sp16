51875f150333f2a9297c00cb7855e0aeda0fa5b3
yuhan <yud039@ucsd.edu>
2016-04-28 17:24:34 -0700

    trying to get the google map to show up in the app

--- app/src/main/java/com/cs110/lit/adventour/MapsActivity.java
+++ app/src/main/java/com/cs110/lit/adventour/MapsActivity.java
@@ -1,7 +1,9 @@
 package com.cs110.lit.adventour;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,9 +19,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        Intent intent = getIntent();
     }
 
     @Override
